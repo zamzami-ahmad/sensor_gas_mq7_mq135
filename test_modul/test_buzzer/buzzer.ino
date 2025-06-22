4c9a8fb04207de106b27fbc8ff983b0f14e73896
Zamzami Ahmad <55796398+zamzami-ahmad@users.noreply.github.com>
2025-06-22 22:34:43 +0800

    Update buzzer.ino

--- test_modul/test_buzzer/buzzer.ino
+++ test_modul/test_buzzer/buzzer.ino
@@ -1,1 +1,16 @@
+int buzzer = 2;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buzzer, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(buzzer, 3000); // A4
+  delay(200);
+
+  noTone(buzzer); // B4
+  delay(200);
+}
 
